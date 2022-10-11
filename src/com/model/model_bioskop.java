585a4756cebb8555d5e60a18a1719e5abcb86b98
andhika3a <detrianugroho@gmail.com>
2022-10-11 21:57:44 +0700

    first model

--- src/com/model/model_bioskop.java
+++ src/com/model/model_bioskop.java
@@ -4,10 +4,38 @@
  */
 package com.model;
 
+import com.controller.controller_bioskop;
+import com.view.Form_Bioskop;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author USER
  */
-public class model_bioskop {
+public class model_bioskop implements controller_bioskop{ 
+
+    @Override
+    public void simpan(Form_Bioskop bioskop) throws SQLException {
+        try {
+            Connection con = koneksi.getcon();
+            String sql = "Insert Into siswa Values (?,?,?,?,?";
+            PreparedStatement prepare = con.preparedStatement(sql);
+            prepare.setString(1, bioskop.cbFilm.getSelectedItem());
+            prepare.setString(2, bioskop.txtbangku.getText();
+            prepare.setString(3, bioskop.chbFb1.getSelectedItem());
+            prepare.setString(4, bioskop.chbFb2.getSelectedItem());
+            prepare.setString(5, bioskop.txtharga.getText());
+            prepare.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Data berhasil diSimpan");
+            prepare.close();
+            Baru(Bioskop);
+        } catch (Exception e) {
+            System.out.println (e);
+        } finally {
+            Tampil(Bioskop);
+            siswa.setLebarKolom();
+        }
+    }
     
 }
